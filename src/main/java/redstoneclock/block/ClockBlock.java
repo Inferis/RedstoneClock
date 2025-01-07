b4c152890a0df1b714e100a837e6f4f918cc9659
Tom <tom@inferis.org>
2025-01-06 22:12:11 -0800

    Turn it into a directional block with one input and multiple outputs.

--- src/main/java/redstoneclock/block/ClockBlock.java
+++ src/main/java/redstoneclock/block/ClockBlock.java
@@ -3,10 +3,14 @@ package redstoneclock.block;
 import java.util.EnumSet;
 import java.util.Set;
 
+import com.mojang.serialization.MapCodec;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
@@ -16,19 +20,12 @@ import net.minecraft.world.World;
 import net.minecraft.world.block.WireOrientation;
 import redstoneclock.RedstoneClock;
 
-public class ClockBlock extends Block implements BlockEntityProvider {
-    private Set<Direction> inputs = EnumSet.noneOf(Direction.class);
-    private Set<Direction> outputs = EnumSet.noneOf(Direction.class);
+public class ClockBlock extends HorizontalFacingBlock implements BlockEntityProvider {
+    private final static MapCodec<ClockBlock> CODEC = Block.createCodec(ClockBlock::new);
 
     public ClockBlock(Settings settings) {
         super(settings);
-
-        setDefaultState(getDefaultState().with(Properties.POWERED, false));
-
-        inputs.add(Direction.EAST);
-        inputs.add(Direction.WEST);
-        outputs.add(Direction.NORTH);
-        outputs.add(Direction.SOUTH);
+        setDefaultState(getDefaultState().with(Properties.POWERED, false).with(Properties.HORIZONTAL_FACING, Direction.NORTH));
     }
 
     @Override
@@ -39,55 +36,29 @@ public class ClockBlock extends Block implements BlockEntityProvider {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(Properties.POWERED);
+        builder.add(Properties.HORIZONTAL_FACING);
     }
+
+    @Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		return super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+	}
 
     @Override
     protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock,
             WireOrientation wireOrientation, boolean notify) {
         super.neighborUpdate(state, world, pos, sourceBlock, wireOrientation, notify);
 
-        RedstoneClock.LOGGER.info("neighborUpdate: " + state + ", " + pos + ", " + sourceBlock);
-
-        world.setBlockState(pos, state.with(Properties.POWERED, false));
-        for (var direction: inputs) {
-            var dirPos = pos.offset(direction);
-            var power = world.getEmittedRedstonePower(dirPos, direction);
-            RedstoneClock.LOGGER.info(direction + " emitted=" + power);
-            if (power > 0) {
-                RedstoneClock.LOGGER.info("POWER ON");
-                world.setBlockState(pos, state.with(Properties.POWERED, true));
-                break;
-            }
-        }
+        var direction = state.get(Properties.HORIZONTAL_FACING);
+        var dirPos = pos.offset(direction);
+        var power = world.getEmittedRedstonePower(dirPos, direction);
+        world.setBlockState(pos, state.with(Properties.POWERED, power > 0));
     }
 
-    // @Override
-    // protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView,
-    //         BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
-    //     BlockState result = super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
-
-    //     RedstoneClock.LOGGER.info("getStateForNeighborUpdate: " + state + ", " + pos + ", " + direction + ", " + neighborPos + ", " + neighborState);
-
-    //     inputs.add(Direction.EAST);
-    //     inputs.add(Direction.WEST);
-
-    //     if (inputs.contains(direction)) {
-    //         var dirPos = pos.offset(direction);
-    //         var power = world.getEmittedRedstonePower(dirPos, direction);
-    //         RedstoneClock.LOGGER.info(direction + " emitted=" + power);
-    //         if (power > 0) {
-    //             RedstoneClock.LOGGER.info("POWER ON");
-    //             return result.with(Properties.POWERED, true);
-    //         }
-    //     }
-
-    //     return result.with(Properties.POWERED, false);
-    // }
-
-    // @Override
-    // protected boolean emitsRedstonePower(BlockState state) {
-    //     return state.get(Properties.POWERED);
-    // }
+    @Override
+    protected boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
 
     @Override
     protected int getStrongRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
@@ -97,18 +68,18 @@ public class ClockBlock extends Block implements BlockEntityProvider {
     @Override
     protected int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
         if (state.get(Properties.POWERED) && world.getBlockEntity(pos) instanceof ClockBlockEntity clockEntity) {
-            Set<Direction> outputs = EnumSet.noneOf(Direction.class);
-            outputs.add(Direction.NORTH);
-            outputs.add(Direction.SOUTH);
-
-            if (outputs.contains(direction)) {
-                RedstoneClock.LOGGER.info("getWeakRedstonePower: " + direction + " powered");
+            var oppositeFacing = state.get(Properties.HORIZONTAL_FACING).getOpposite();
+            if (direction != Direction.UP && direction != Direction.DOWN && direction != oppositeFacing) {
                 return 15;
             }
         }
 
-        RedstoneClock.LOGGER.info("getWeakRedstonePower: " + direction + " unpowered");
         return 0;
+    }
+
+    @Override
+    protected MapCodec<? extends HorizontalFacingBlock> getCodec() {
+        return CODEC;
     }
 }
 
