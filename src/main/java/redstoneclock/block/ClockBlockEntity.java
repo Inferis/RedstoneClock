87b1f04b5855bd656de83a44c64b027d1d9a6585
Tom <tom@inferis.org>
2025-01-08 10:14:05 -0800

    read/write nbt in ClockBlockEntity.

--- src/main/java/redstoneclock/block/ClockBlockEntity.java
+++ src/main/java/redstoneclock/block/ClockBlockEntity.java
@@ -2,17 +2,21 @@ package redstoneclock.block;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryWrapper.WrapperLookup;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import redstoneclock.networking.OpenEditorS2CPayload;
 
 public class ClockBlockEntity extends BlockEntity {
+    private final String ACTIVE_INTERVAL_KEY = "active_interval";
+    private final String INACTIVE_INTERVAL_KEY = "inactive_interval";
+    
     private int signalStrength = 15;
     private int cycle = 0;
-    private int activeInterval = 6;
-    private int inactiveInterval = 10;
+    private int activeInterval = 20;
+    private int inactiveInterval = 20;
 
     public ClockBlockEntity(BlockPos pos, BlockState state) {
         super(RedstoneClockBlockEntities.CLOCK_ENTITY, pos, state);
@@ -49,5 +53,26 @@ public class ClockBlockEntity extends BlockEntity {
     public void updateIntervals(int activeInterval, int inactiveInterval) {
         this.activeInterval = activeInterval;
         this.inactiveInterval = inactiveInterval;
+        this.markDirty();
+    }
+
+    @Override
+    protected void readNbt(NbtCompound nbt, WrapperLookup registries) {
+        super.readNbt(nbt, registries);
+
+        if (nbt.contains(ACTIVE_INTERVAL_KEY)) {
+            activeInterval = nbt.getInt(ACTIVE_INTERVAL_KEY);
+        }
+        if (nbt.contains(INACTIVE_INTERVAL_KEY)) {
+            inactiveInterval = nbt.getInt(INACTIVE_INTERVAL_KEY);
+        }
+    }
+    
+    @Override
+    protected void writeNbt(NbtCompound nbt, WrapperLookup registries) {
+        super.writeNbt(nbt, registries);
+
+        nbt.putInt(ACTIVE_INTERVAL_KEY, activeInterval);
+        nbt.putInt(INACTIVE_INTERVAL_KEY, inactiveInterval);
     }
 }
